8a57f27663e1579d1e3b8c06a5f8f3c917e2a0c6
Martin <martinvalchev2@gmail.com>
2018-11-21 17:51:04 +0200

    create login bar, navbar - middle

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,49 @@ var $ = require('gulp-load-plugins')();
 var path = {
 	SCSS_SRC	: './scss/**/*.scss',
 	SCSS_DST	: './css',
-	HTML_SRC	: ['./css/*.css','./*.html','./_posts/*.*','./_layouts/*.*', './_includes/*.*'],
+	CSS_JKDST	: './docs/css',
+	HTML_SRC	: ['./*.html','./_post/*.*','./_layouts/*.*', './_includes/*.*'],
 }
 
 gulp.task('scss', function () {
 	
 	gulp.src( path.SCSS_SRC )
-	    .pipe($.sass())
-	    .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-	    .pipe($.size({ showFiles: true }))
-	    .pipe($.csso())
-	    .pipe($.size({ showFiles: true }))
-	    .pipe($.sourcemaps.write('map'))
-	    .pipe(gulp.dest( path.SCSS_DST ))
-	    .pipe(browserSync.stream({ match: '**/*.css' }))
+	.pipe($.sourcemaps.init())
+	.pipe($.sass())
+	.pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+	.pipe($.size({ showFiles: true }))
+	.pipe($.csso())
+	.pipe($.size({ showFiles: true }))
+	.pipe($.sourcemaps.write('map'))
+	.pipe(gulp.dest( path.SCSS_DST ))
+	.pipe(gulp.dest( path.CSS_JKDST ))
+	.pipe(browserSync.stream({ match: '**/*.css' }))
 	;
 	
 });
 
 gulp.task('jekyll', function () {
-	require('child_process').exec('bundle exec jekyll build --baseurl=', {stdio: 'inherit'});
+	require('child_process').exec('jekyll build --baseurl=', {stdio: 'inherit'}, function(){
+		browserSync.reload();
+	});
 });
 
 gulp.task('serve', function() {
 	
-	browserSync.init({
-		server: {
-			baseDir: "./docs/"
-		}
-	});
+	// browserSync.init({
+	// 	proxy: {
+	// 		target: "http://127.0.0.1:4000/"
+	// 	}
+	// });
 	
-	gulp.watch(path.SCSS_SRC, ['scss', 'jekyll']);
-	gulp.watch(path.HTML_SRC, ['jekyll']);
+	//browserSync.init({
+		//server: {
+			//baseDir: "./docs/"
+		//}
+	//});
+	
+	gulp.watch(path.SCSS_SRC, ['scss']);
+	// gulp.watch(path.HTML_SRC, ['jekyll']);
 	gulp.watch(path.HTML_SRC).on('change', browserSync.reload);
 	
 });
